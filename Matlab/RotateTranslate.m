2005e26ab3f02f783b593b440faae419b8b51329
Ørjan Mæhre <orjaaan@gmail.com>
2016-02-02 19:45:27 +0100

    Implementet rotation and translation matix
    Implementet rotation and translation matix

--- Matlab/RotateTranslate.m
+++ Matlab/RotateTranslate.m
@@ -1,11 +1,20 @@
-%% This script is used to rotate and translate the position data from 
+%% 
+% This script is used to rotate and translate the position data from 
 % the robot. The position from the sensor is related to the wobj
 % but the position data from the robot is related to the base.
 %%
+clear all; 
 
+% x, y and z coordinates for origo in the new cord.system.
 vx = 83.663994925;
 vy = -716.879172118;
 vz = 531.771925931;
+
+degreesToRad = pi/180;
+
+theta = 135*degreesToRad;
+gamma = 45 *degreesToRad; 
+tau = 0; 
 
 %Open file
 fileId = fopen('position.txt');
@@ -16,35 +25,23 @@ fclose(fileId);
 
 %Extract the position data and convert from string to double
 for i = 1: 1: size(C{1,1},1)
-    midTime = C{1,1}{i,1}; 
-    time(i) = str2double(midTime);
+    tempTime = C{1,1}{i,1}; 
+    time(i) = str2double(tempTime);
 end
 
-for i = 1: 1: size(C{1,2},1)
-    midSentX = C{1,2}{i,1};
-    sentX(i) = str2double(midSentX);
+for j = 2: 1: 3
+    for i = 1: 1: size(C{1,2},1)
+        tempSentSensorXY = C{1,j}{i,1};
+        sentSensorXY(i,j-1) = str2double(tempSentSensorXY);
+    end
 end
 
-for i = 1: 1: size(C{1,3},1)
-    midSentY = C{1,3}{i,1};
-    sentY(i) = str2double(midSentY);
+for j = 4: 1: 6 
+    for i = 1: 1: size(C{1,j},1)
+        tempRobotXYZ = C{1,j}{i,1};
+        robotXYZ(i, j-3) = str2double(tempRobotXYZ);
+    end
 end
-
-for i = 1: 1: size(C{1,4},1)
-    midRobotX = C{1,4}{i,1};
-    robotX(i) = str2double(midRobotX);
-end
-
-for i = 1: 1: size(C{1,5},1)
-    midRobotY = C{1,5}{i,1};
-    robotY(i) = str2double(midRobotY);
-end
-
-for i = 1: 1: size(C{1,6},1)
-    midRobotZ = C{1,6}{i,1};
-    robotZ(i) = str2double(midRobotZ);
-end
-
 
 %Rotation matrices
 rotZ = [cos(theta), -sin(theta), 0: sin(theta), cos(theta), 0: 0, 0, 1];
@@ -54,4 +51,6 @@ rotX = [1, 0, 0: 0, cos(tau), -sin(tau): 0, sin(tau), cos(tau)];
 
 %translation matrix
 translation = [1, 0, 0, vx : 0, 1, 0, vy : 0, 0, 1, vz : 0,0,0,1];
+
+
 
