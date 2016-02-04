4bdafe8f164185306badb0187031fcd68c956478
Ørjan Mæhre <orjaaan@gmail.com>
2016-02-04 17:14:12 +0100

    Translation scrips writes translated pos to file and plot the data

--- Matlab/RotateTranslate.m
+++ Matlab/RotateTranslate.m
@@ -2,16 +2,21 @@
 % This script is used to transform the position data of the disk from the 
 % workobjects coordinate system to the robot base cord.system. 
 %%
+clc;
 clear all; 
+
+cx = 1;
+cy = 2;
+cz = 3; 
 
 % x, y and z coordinates for origo in the new cord.system.
 TransX = 83.663994925;
 TransY = -716.879172118;
-TransZ = 531.771925931;
+TransZ = 700.771925931;
 
 % The rotation angles (same as in RS)
 theta = -30; 
-gamma = 150;
+gamma = 120; % 180- angle of the ramp
 tau = 0; 
 
 %Open file
@@ -22,13 +27,13 @@ C = textscan(fileId, '%s %s %s %s %s %s %s');
 fclose(fileId);
 
 %Extract the position data and convert from string to double
-for i = 1: 1: size(C{1,1},1)
+for i = 1: 1: size(C{1,7},1)
     tempTime = C{1,1}{i,1}; 
     time(i) = str2double(strrep(tempTime, ',' , '.'));
 end
 
 for j = 2: 1: 4
-    for i = 1: 1: size(C{1,2},1)
+    for i = 1: 1: size(C{1,7},1)
             tempSentSensorXYZ = C{1,j}{i,1};
             sentSensorXYZ(i,j-1) = str2double(strrep(tempSentSensorXYZ, ',' , '.')); 
     
@@ -36,13 +41,13 @@ for j = 2: 1: 4
 end
 
 for j = 5: 1: 7 
-    for i = 1: 1: size(C{1,j},1)
+    for i = 1: 1: size(C{1,7},1)
         tempRobotXYZ = C{1,j}{i,1};
         robotXYZ(i, j-4) = str2double(strrep(tempRobotXYZ, ',' , '.'));
     end
 end
 
-for i = 1 : 1 : size(C{1,2},1)
+for i = 1 : 1 : size(C{1,7},1)
     sentSensorXYZ(i, 4) = 1;
 end
 
@@ -69,9 +74,44 @@ for i = 1: 1 : size(sentSensorXYZ,1)
    
 end
 
+% Plotting the data
+figure;
+plot(time, newXYZcord(1:size(newXYZcord,1),cx),'-r');hold on; 
+plot(time, robotXYZ(1:size(robotXYZ,1),cx),'-b'); hold on; 
 
 
+plot(time, newXYZcord(1:size(newXYZcord,1),cy),'-k'); hold on; 
+plot(time, robotXYZ(1:size(robotXYZ,1),cy),'-g');hold on;
 
+plot(time, newXYZcord(1:size(newXYZcord,1),cz),'-m'); hold on; 
+plot(time, robotXYZ(1:size(robotXYZ,1),cz),'-c');
 
+legend('Disc X position','Robot X position','Disc Y position','Robot Y position','Disc Z position','Robot Z position' )
+xlim([0 1.5])
+xlabel('Time [s]')
+ylabel('Position [mm]')
+grid on; 
+ 
+% Finding the biggest differense between rob.pos and disc.pos
+[maxDiffX,cellNumX] = max(abs(newXYZcord(1:size(newXYZcord,1),cx))- abs(robotXYZ(1:size(robotXYZ,1),cx)));
 
+[maxDiffY,cellNumY] = max(abs(newXYZcord(1:size(newXYZcord,1),cy))- abs(robotXYZ(1:size(robotXYZ,1),cy)));
+
+[maxDiffZ,cellNumZ] = max(abs(newXYZcord(1:size(newXYZcord,1),cz))- abs(robotXYZ(1:size(robotXYZ,1),cz)));
+
+X = 'X:';
+T = ' T:';
+Y = ' Y:';
+Z = ' Z:';
+
+fprintf('%s %#.4g %s %#.4g %s %#.4g %s %#.4g %s %#.4g %s %#.4g ', X, maxDiffX,T, time(1,cellNumX),Y,maxDiffY, T, time(1,cellNumY), Z,maxDiffZ,T ,time(1,cellNumZ));
+
+% Writing the processed data to txt file.
+% fileIDW = fopen('Test\16.02.03 60deg Test5(P).txt','wt');
+% fprintf(fileIDW, 'Time \t\t RobotX \t RobotY \t RobotZ \t DiscX \t\t DiscY \t\t DiscZ \n')
+% for i = 1 :1 : size(robotXYZ,1)
+%         fprintf(fileIDW,'%#.6g \t',(time(:,i)),robotXYZ(i,:), newXYZcord(i,:));
+%         fprintf(fileIDW, '\n');
+% end
+% fclose('all');
 
