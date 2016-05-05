f78b4a97218e43f0df5c302dcc7be837aa6f720a
Ørjan Mæhre <orjaaan@gmail.com>
2016-05-05 12:19:10 +0200

    Changed folder structure

---
+++ test/EgmTest/RotateTranslateTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExternalGuidedMotion;
+using MathNet.Numerics.LinearAlgebra;
+
+
+namespace EgmTest
+{
+    [TestClass]
+    public class RotateTranslateTest
+    {
+        public Vector<double> newCord = Vector<double>.Build.Dense(4);
+        [TestMethod]
+        public void IsRotateTranslateCorrect()
+        {
+            double X = 0;
+            double Y = 0;
+            double Z = 0;
+
+            RotateTranslate FromWobjToBase = new RotateTranslate(X,Y,Z);
+            newCord = FromWobjToBase.RotatedTranslatedCord();
+            Debug.WriteLine(newCord.ToString());
+
+        }
+    }
+}
---
+++ test/EgmTest/TestOfModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExternalGuidedMotion;
+
+namespace EgmTest
+{
+    [TestClass]
+    public class TestOfModel
+    {
+        private double _position;
+        private double _accel;
+        private double _vel;
+
+        [TestMethod]
+        public void ModelTest()
+        {
+            double angel = 30;
+            double g = 9.81;
+            double time = 0.8;
+            double friction = 0.5;
+
+            ModelBasedPrediction testModel = new ModelBasedPrediction(angel, g, time, friction);
+
+            _vel = testModel.Velocity();
+            _accel = testModel.Acceleration();
+            _position = testModel.Dist;
+            Debug.WriteLine("Postition " + _position.ToString() + " Vel " +  _vel.ToString() + " Accel " + _accel.ToString());
+        }
+    }
+}
