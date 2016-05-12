1b6a9a0e264ca318c07653537eace73d54048b1e
orjanmehre <orjaaan@gmail.com>
2016-05-12 18:00:07 +0200

    Need a better way to predict

--- src/Predictor.cs
+++ src/Predictor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ExternalGuidedMotion
 {
@@ -14,10 +11,16 @@ namespace ExternalGuidedMotion
         private double _currentPosition; 
         private double _prevVelocity;
         private double _currentVelocity;
+        private double _prevPrevVelocity;
+        private double _prevPrevPrevVelocity;
+        private double _prevPrevPrevPrevVelocity;
         public double _a { get; set; }
         private double _prevTime;
         private double _currentTime;
         public double _error { get; set; }
+        public TextWriter ExecutionTime = new StreamWriter(@"..\...\Test.txt", true);
+
+        private double _regCoeff;
 
         public double PredictedPosition { get; set; }
        
@@ -25,8 +28,13 @@ namespace ExternalGuidedMotion
         {
             _prevPosition = 0;
             _prevTime = 0;
-            _prevVelocity = 0;
+            _prevVelocity = 1;
             _error = 0;
+            _regCoeff = 1;
+            _prevPrevVelocity = 1;
+            _prevPrevPrevVelocity = 1;
+            _prevPrevPrevPrevVelocity = 1;
+
         }
 
         public void NewPrediction(double time, double position)
@@ -34,20 +42,29 @@ namespace ExternalGuidedMotion
             this._currentTime = time;
             this._currentPosition = position;
             setError();
+            regressionCoeffisient();
             predictedPosition();
             _a = acceleration();
             setPrevPosition();
-            setPrevVelocity();
+            setPrevVelocitys();
+            WriteExecutionTimeToFile();
         }
 
+        public void WriteExecutionTimeToFile()
+        {
+            ExecutionTime.WriteLine(_currentPosition.ToString() + " " +  _prevPosition.ToString() + " " + _currentTime.ToString());
+        }
 
         private void setPrevPosition()
         {
             _prevPosition = _currentPosition;
         }
 
-        private void setPrevVelocity()
+        private void setPrevVelocitys()
         {
+            _prevPrevPrevPrevVelocity = _prevPrevPrevVelocity;
+            _prevPrevPrevVelocity = _prevPrevVelocity;
+            _prevPrevVelocity = _prevVelocity;
             _prevVelocity = _currentVelocity;
         }
 
@@ -68,7 +85,7 @@ namespace ExternalGuidedMotion
 
         private void predictedPosition()
         {
-            PredictedPosition = _currentPosition + velocity()*_currentTime + _error;
+            PredictedPosition = _currentPosition + _regCoeff*velocity()*_currentTime;
             
         }
 
@@ -79,8 +96,14 @@ namespace ExternalGuidedMotion
             if (_error < 0 || _error > 100)
             {
                 _error = 0;
-                Console.WriteLine("Error has an unnatural value");
+                //Console.WriteLine("Error has an unnatural value");
             }
+        }
+
+        private void regressionCoeffisient()
+        {
+            _regCoeff = Math.Abs(_currentVelocity / _prevVelocity);
+            Debug.WriteLine("RegCoeff: " + _regCoeff.ToString());
         }
     }
 }
